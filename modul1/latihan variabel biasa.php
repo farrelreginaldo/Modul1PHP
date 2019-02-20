1ecda40d07c9bead4d7dba5edd750824deb86156
farrelreginaldo <41880009+farrelreginaldo@users.noreply.github.com>
2019-02-20 14:11:27 +0700

    Add files via upload

---
+++ modul1/latihan variabel biasa.php
@@ -0,0 +1,5 @@
+<?php $sekolah = "SMK Telkom Malang";
+$angkatan = 24;
+ echo "saya adalah siswa ".$sekolah;
+ echo " angkatan ".$angkatan;
+ ?> 
---
+++ modul1/latihan variabel dalam kelompok.php
@@ -0,0 +1,8 @@
+<?php
+$nama = "Regin";
+function tampil_nama(){
+    global $nama;
+    echo "Nama : ".$nama;
+}
+tampil_nama();
+?>
